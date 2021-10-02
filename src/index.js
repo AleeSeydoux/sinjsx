e33bbf75e850cf3359a718590b262da63067805a
Mayra Osorio <aleevelarde9@gmail.com>
2021-10-02 01:42:07 -0500

    correcciones

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+
+//react sinJSX
+
+const titulo = React.createElement(
+  'h1',
+  {id: 'titulo', className: 'encabezado'},
+  'Hola Mundo' 
+
+)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  titulo,
   document.getElementById('root')
 );
 
